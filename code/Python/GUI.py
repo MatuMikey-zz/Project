84fbd166f8df59bbd67696f6d9d8b715484ad503
Michael <s11093804@gmail.com>
2018-10-17 01:22:46 +0200

    GUI completed to my own satisfaction

--- code/Python/GUI.py
+++ code/Python/GUI.py
@@ -4,24 +4,36 @@ root = Tk()
 
 import tkinter.ttk
 
-tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=0, rowspan=7, sticky='ns')
-tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=3, row = 0, rowspan=7, sticky='ns')
-tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=5, row = 0, rowspan=7, sticky='ns')
-
-tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=1, columnspan=8, sticky='ew')
-tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=3, columnspan=8, sticky='ew')
-tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=5, columnspan=8, sticky='ew')
+tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=1, row=0, rowspan=8, sticky='ns')
+tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=3, row = 0, rowspan=8, sticky='ns')
+tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=5, row = 0, rowspan=8, sticky='ns')
+tkinter.ttk.Separator(root, orient=VERTICAL).grid(column=7, row = 0, rowspan=8, sticky='ns')
 
 
+
+tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0 ,row=1, columnspan=10, sticky='ew')
+tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=1, row=3, columnspan=10, sticky='ew')
+tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=1, row=5, columnspan=10, sticky='ew')
+tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=7, columnspan=10, sticky='ew')
+tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=8, columnspan=10, sticky='ew')
+
+
+
+#Labels
 sensorNumberLabel= Label(root, text="Sensor #", font = "Helvetica 10 bold").grid(row=0, column=0)
 sensorReadingLabel = Label(root, text="Sensor Temperature", font = "Helvetica 10 bold").grid(row=0, column=2)
 sensorNeuralNetworkLabel = Label(root, text="Neural Network Temperature", font = "Helvetica 10 bold").grid(row=0,column=4)
-sensorOnlineLabel = Label(root, text="Sensor Online", font = "Helvetica 10 bold").grid(row=0,column=6)
+sensorErrorDifferenceLabel = Label(root, text="Error %", font = "Helvetica 10 bold").grid(row=0,column=6)
+sensorOnlineLabel = Label(root, text="Sensor Online", font = "Helvetica 10 bold").grid(row=0,column=8)
 
 sensor1Label = Label(root, text="Sensor 1").grid(row=2,column=0)
 sensor2Label = Label(root, text="Sensor 2").grid(row=4, column=0)
 sensor3Label = Label(root, text="Sensor 3").grid(row=6, column=0)
-
+historicalLabel = Label(root, text="Historical data", font = "Helvetica 10 bold").grid(row=9,column=0)
+#Buttons
+halfdayButton = Button(root, text="12 Hours", font = "Helvetica 10 bold").grid(row=9, column=2)
+dayButton = Button(root, text="24 Hours", font = "Helvetica 10 bold").grid(row=9, column=3)
+allTimeButton = Button(root, text = "All time data", font = "Helvetica 10 bold"). grid(row=9, column=4)
 
 while 1:
     root.update()
