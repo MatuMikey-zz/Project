5931bb2e77501a1e89c9c75bb82341bfb5833c76
Michael <s11093804@gmail.com>
2018-10-25 00:31:08 +0200

    Began neural network section

--- Code/Python/plotTemperatureData.py
+++ Code/Python/plotTemperatureData.py
@@ -1,6 +1,6 @@
 import csv
 from KalmanFilter import KalmanFilter
-with open('SmallHouseTest2.csv', 'r') as csvfile:
+with open('SmallHouse.csv', 'r') as csvfile:
     so = csv.reader(csvfile, delimiter=';', quotechar='"')
     so = list(so)
 import matplotlib.pyplot as plt
@@ -12,9 +12,9 @@ def meanFilter(array):
     return newArray
 
 
-kFilter1 = KalmanFilter(1,1,0.01)
-kFilter2 = KalmanFilter(1,1,0.01)
-kFilter3 = KalmanFilter(1,1,0.01)
+kFilter1 = KalmanFilter(1,1,0.1)
+kFilter2 = KalmanFilter(1,1,0.1)
+kFilter3 = KalmanFilter(1,1,1)
 sensor1=[]
 sensor2=[]
 sensor3=[]
@@ -22,32 +22,36 @@ time=[]
 #for i in range(1, len(so)):
     #if (so[i][2]  == 0.0 or so[i][3] == 0.0 or so[i][4] == 0.0):
      #   so.pop(i)
-for i in range(5, len(so)):
+for i in range(7500, len(so)):
     time.append(float(so[i][1]))    
     sensor1.append(float(so[i][2]))
     sensor2.append(float(so[i][3]))
     sensor3.append(float(so[i][4]))
 
 
-plt.subplot(2,2,1)
-plt.plot(sensor1)
-plt.subplot(2,2,2)
-plt.plot(sensor2)
-plt.subplot(2,2,3)
-plt.plot(sensor3)
+#plt.subplot(2,2,1)
+#plt.plot(sensor1)
+#plt.subplot(2,2,2)
+#plt.plot(sensor2)
+#plt.subplot(2,2,3)
+plt.plot(sensor3, label="Raw sensor data")
 
 for i in range(0, len(sensor1)):
     sensor1[i] = kFilter1.updateEstimate(sensor1[i])
     sensor2[i] = kFilter2.updateEstimate(sensor2[i])
     sensor3[i] = kFilter3.updateEstimate(sensor3[i])
 
-plt.subplot(2,2,1)
-plt.plot(sensor1)
-plt.subplot(2,2,2)
-plt.plot(sensor2)
-plt.subplot(2,2,3)
-plt.plot(sensor3)
-   
+#plt.subplot(2,2,1)
+#plt.plot(sensor1)
+#plt.subplot(2,2,2)
+#plt.plot(sensor2)
+#plt.subplot(2,2,3)
+plt.plot(sensor3, label="Kalman filtered data")
+plt.legend(loc='upper left')
+plt.ylim([-1.0,40])
+plt.title("Raw sensor data and kalman filtered data with Q = 1")
+plt.xlabel("Sensor reading number")
+plt.ylabel("Temperature in °C")
 #plt.plot(sensor3)
 
 
