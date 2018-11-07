e3b12ba562ed2b547c9f47e17fe305939060f231
Michael <s11093804@gmail.com>
2018-11-07 13:16:05 +0200

    latest

--- Code/Python/plotTemperatureData.py
+++ Code/Python/plotTemperatureData.py
@@ -20,9 +20,9 @@ sensor1=[]
 sensor2=[]
 sensor3=[]
 time=[]
-#for i in range(1, len(so)):
-    #if (so[i][2]  == 0.0 or so[i][3] == 0.0 or so[i][4] == 0.0):
-     #   so.pop(i)
+for i in range(1, len(so)):
+    if (so[i][2]  == 0.0 or so[i][3] == 0.0 or so[i][4] == 0.0):
+        so.pop(i)
 for i in range(2, len(so)):
     time.append(float(so[i][1]))    
     sensor1.append(float(so[i][2]))
@@ -31,11 +31,11 @@ for i in range(2, len(so)):
 
 
 plt.subplot(2,2,1)
-plt.plot(sensor1)
+#plt.plot(sensor1)
 plt.subplot(2,2,2)
-plt.plot(sensor2)
+#plt.plot(sensor2)
 plt.subplot(2,2,3)
-plt.plot(sensor3)
+#plt.plot(sensor3)
 #addseconds = []
 #k = 0
 #for i in range(0, len(time)):
